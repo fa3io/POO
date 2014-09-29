972a1521b62fc6ccea50e7a88fb3df0b30e83ed5
Fabio - Holding <fa3io.santos.alves@gmail.com>
2014-09-29 12:04:54 -0300

    Implementação do ExercicioSimples10 calculando o fatorial e a sequencia fibonacci

--- src/aula1/ExercicioSimples06.java
+++ src/aula1/ExercicioSimples06.java
@@ -7,17 +7,20 @@ public class ExercicioSimples06 {
 	public static void main(String[] args) {
 
 		Scanner Scanner = new Scanner(System.in);
-		int num, fat = 1;
+		int numero, fatorial = 1;
 
-		System.out.println("Digite um nº");
-		num = Scanner.nextInt();
+		System.out.println("Digite um numero");
+		numero = Scanner.nextInt();
 
-		for (int i = num; i > 0; i--) {
-			fat = fat * i;
+		for (int i = 0; i <=numero; i++) {
 			
-			System.out.println(fat);
+			if(i == 0){
+				System.out.println(i+"! ="+fatorial);
+			}else{
+				fatorial = fatorial * i;
+				System.out.println(i+"! ="+fatorial);
+			}	
 		}
 
-		System.out.println("!" + num + " = " + fat);
 	}
 }
---
+++ src/aula1/ExercicioSimples10.java
@@ -0,0 +1,34 @@
+package aula1;
+
+import java.util.Scanner;
+
+public class ExercicioSimples10 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Digite um numero: ");
+		int numero = scanner.nextInt();
+
+		System.out.println("Fatorial de " + numero + " = " + fat(numero));
+		System.out.println("Seq. de Fibonacci, elemento na posica " + numero + " = "
+				+ fib(numero));
+		System.out.println("===============================================");
+	}
+
+	public static long fat(int n) {
+		if (n == 0)
+			return (1);
+		else
+			return (n * fat(n - 1));
+	}
+
+	public static int fib(int n) {
+		if ((n == 1) || (n == 2))
+			return (1);
+		else
+			return (fib(n - 1) + fib(n - 2));
+
+	}
+}

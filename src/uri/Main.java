f34591c64345f44b3580602f8bba6c394acbfaea
Fabio - Holding <fa3io.santos.alves@gmail.com>
2014-09-29 17:01:20 -0300

    Inicio da implementação do Exercicio referente a media ponderada.

--- src/uri/Main.java
+++ src/uri/Main.java
@@ -1,8 +1,20 @@
 package uri;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		 Locale.setDefault(Locale.US);
+		
+		double a ,b, soma ;
+		Scanner teclado = new Scanner (System.in);
+		
+		a = teclado.nextDouble();
+		b = teclado.nextDouble();
+		
 		
 	}
 

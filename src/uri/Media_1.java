a6cfb4e2e3771517d0dfe3d6710f51220aa54872
Fabio - Holding <fa3io.santos.alves@gmail.com>
2014-09-30 08:39:38 -0300

    Conclusão do codigo de Media_1 do URI

--- src/uri/Media_1.java
+++ src/uri/Media_1.java
@@ -3,17 +3,21 @@ package uri;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class Media_1 {
 	
 	public static void main(String[] args) {
 		
 		 Locale.setDefault(Locale.US);
 		
-		double a ,b, soma ;
+		double a ,b, media ;
 		Scanner teclado = new Scanner (System.in);
 		
 		a = teclado.nextDouble();
 		b = teclado.nextDouble();
+		
+		 media = ((a * 3.5f) + (b * 7.5f)) / 11;
+         
+         System.out.printf("MEDIA = %.5f\n", media);
 		
 		
 	}

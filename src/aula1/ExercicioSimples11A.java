32feaa834d2a303d6832b81de001db9fe34a817e
Fabio Santos <fa3io.santos.alves@gmail.com>
2014-10-13 12:07:26 -0300

    Alteração em exercicio 11 aula 1

--- src/aula1/ExercicioSimples11A.java
+++ src/aula1/ExercicioSimples11A.java
@@ -4,15 +4,31 @@ public class ExercicioSimples11A {
 
 	public static void main(String[] args) {
 		
-		System.out.println(soma(4,7));
+		System.out.println(soma(1,3));
 	}
 
 	private static int soma(int i, int j) {
 		if(i == 0){
-			return i;
+			return j ;
+		}else if(j ==1 ) {
+			return 1;
 		}else {
 			return soma(i, j-1)+ 1;
 		}
 	}
 
 }
+
+
+/*
+
+1 3 
+1 2
+1 1 
+
+
+
+
+
+
+*/

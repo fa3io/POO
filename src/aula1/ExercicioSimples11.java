8475c60fe52636a179aea4e17a4d2bf70254c25c
Fabio Santos <fa3io.santos.alves@gmail.com>
2014-10-20 14:03:21 -0200

    Termino da implementação do ExercicioSimples11 referente a recursividade.

--- src/aula1/ExercicioSimples11.java
+++ src/aula1/ExercicioSimples11.java
@@ -9,52 +9,63 @@ public class ExercicioSimples11 {
 		int numero1, numero2, resultado;
 		String operacao;
 
-		System.out.println("Digite a operação que deseja realizar: ");
-		System.out.println("+ = Adição");
-		System.out.println("- = Subtração");
-		System.out.println("* = Muntiplicação");
-		System.out.println("/ = Divisão ");
+		System.out.println("Digite a operacaoo que deseja realizar: ");
+		System.out.println("+ = Adicaoo");
+		System.out.println("- = Subtracao");
+		System.out.println("* = Muntiplicacao");
+		System.out.println("/ = Divisao ");
 		System.out.println("_______________________________________");
 		operacao = scanner.nextLine();
-		System.out.println("Digite o 1º Numero:");
+		System.out.println("Digite o 1 Numero:");
 		numero1 = scanner.nextInt();
-		System.out.println("Digite o 2º Numero:");
+		System.out.println("Digite o 2 Numero:");
 		numero2 = scanner.nextInt();
 		System.out.println("_______________________________________");
 
 		if (operacao.equals("+")) {
-			soma(numero1, numero2);
+			System.out.println(soma(numero1, numero2));
 		} else if (operacao.equals("-")) {
-			subtracao(numero1, numero2);
+			System.out.println(subtracao(numero1, numero2));
 		} else if (operacao.equals("*")) {
-			multiplicacao(numero1, numero2);
+			System.out.println(multiplicacao(numero1, numero2));
 		} else if (operacao.equals("/")) {
-			divisao(numero1, numero2);
+			System.out.println(divisao(numero1, numero2));
 		} else {
-			System.out.println("Operação escolhida é inválida!");
+			System.out.println("Operacao escolhida eh invalida!");
 		}
 	}
 
 	private static int divisao(int numero1, int numero2) {
-		return 0;
+		if(numero1 < numero2){
+                    return 0;
+                }else{
+                    return 1 + divisao(numero1 - numero2, numero2);
+                    
+                }
 	}
 
 	private static int multiplicacao(int numero1, int numero2) {
-		return 0;
-
+		if(numero2 == 1){
+			return numero1;
+		}else {
+                    return numero1 + multiplicacao(numero1, numero2 - 1);
+		} 
 	}
 
 	private static int subtracao(int numero1, int numero2) {
-		return 0;
+            if(numero2 == 0){
+                return numero1;
+            }else{
+               return subtracao(numero1, numero2 - 1) - 1;
+            }
 	}
 
 	private static int soma(int numero1, int numero2) {
-		if((numero1 == 1)){
-			return numero2;
-		}else if((numero2 == 1)){
-			return numero1;
-		}else{
-			return 0;
+		if(numero2 == 0){
+			return numero1 ;
+		
+		}else {
+                    return soma(numero1, numero2 - 1)+ 1;
 		}
 		
 	}
